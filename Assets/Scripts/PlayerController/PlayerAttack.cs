22b9bbed1469fe488c53f7264027045dd9ae2851
Definer <1780389351@qq.com>
2023-09-27 10:33:30 +0800

    fix(consle): 修复了命令行输出中文报错的bug
    -消除了中文报错的log，修改了字体资源（TODO：加入中文支持的TMP字体资源）

--- Assets/Scripts/PlayerController/PlayerAttack.cs
+++ Assets/Scripts/PlayerController/PlayerAttack.cs
@@ -1,6 +1,43 @@
+using System;
 using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
-    
+    private Animator anim;
+    private int m_currentAttack = 0;
+    private float m_timeSinceAttack = 0.0f;
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        Attack();
+        m_timeSinceAttack += Time.deltaTime;
+        if (m_timeSinceAttack > 1.0f)
+            m_currentAttack = 1;
+    }
+
+    void Attack()
+    {
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            m_currentAttack++;
+
+            // Loop back to one after third attack
+            if (m_currentAttack > 3)
+                m_currentAttack = 1;
+
+            // Reset Attack combo if time since last attack is too large
+            if (m_timeSinceAttack > 1.0f)
+                m_currentAttack = 1;
+
+            // Call one of three attack animations "Attack1", "Attack2", "Attack3"
+            anim.SetTrigger("Attack" + m_currentAttack);
+
+            // Reset timer
+            m_timeSinceAttack = 0.0f;
+        }
+    }
 }
