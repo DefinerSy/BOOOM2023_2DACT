352dd961da1ed10cb35b68e7aee129934f17b257
GanYuanmomo <2356680693@qq.com>
2023-10-10 11:09:47 +0800

    feat(ui):新增敌人血条跟随功能
    Signed-off-by: DESKTOP-4JK5UM1\23566 <2356680693@qq.com>

---
+++ Assets/Scripts/UI/EnemyBarFollow.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBarFollow : MonoBehaviour
+{
+    public GameObject enemy;
+
+    public GameObject enemyBar;
+
+    public float y;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemyBar.transform.position = enemy.transform.position + new Vector3(0,y);
+    }
+}
