ac97b761a77cba158b86bd5ab5a12789c93e3e96
Lin Zifeng <2100507232@gdpu.edu.cn>
2023-10-11 21:17:20 +0800

    fix(enemy): 更新了敌人Hurt动画(修复)

--- Assets/Scripts/Enemy/Task/Attack.cs
+++ Assets/Scripts/Enemy/Task/Attack.cs
@@ -14,18 +14,17 @@ public class Attack : EnemyAction
     public override TaskStatus OnUpdate()
     {
         stateInfo = Animator.GetCurrentAnimatorStateInfo(0);
+        if(stateInfo.IsName("Hurt"))
+        {
+            return TaskStatus.Running;
+        }
         Animator.Play("Attack");
         if (stateInfo.normalizedTime > 0.99f&&stateInfo.IsName("Attack"))
         {
             return TaskStatus.Success;
         }
-        if(stateInfo.IsName("Hurt"))
-        {
-            return TaskStatus.Running;
-        }
 
         return TaskStatus.Running;
-
     }
     
     public override void OnEnd()
