f7032d2cc51a3d82334dff3043f1b0be6505dd15
Lin Zifeng <2100507232@gdpu.edu.cn>
2023-10-11 22:22:17 +0800

    feat(enemy): 更新了敌人Die动画(追加)

--- Assets/EnemyAnimationController.cs
+++ Assets/EnemyAnimationController.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class EnemyAnimationController : MonoBehaviour
 {
     GameObject _character;
+    
     
     private void Awake()
     {
--- Assets/Scripts/Character/EnermyCharacter.cs
+++ Assets/Scripts/Character/EnermyCharacter.cs
@@ -1,17 +1,19 @@
 using QFSW.QC;
 using UnityEngine;
-
+using BehaviorDesigner.Runtime.Tasks;
 public class EnermyCharacter : Character
 {
     [Header("架势条")]
     [SerializeField] float _maxPosture;
     [SerializeField] float _currentPosture;
     private Animator _animator;
+    private Behaviour _behaviour;
     protected override void Start()
     {
         _animator = GetComponentInChildren<Animator>();
         base.Start();
         _currentPosture = _maxPosture;
+        _behaviour=GetComponentInParent<Behaviour>();
     }
     
     
@@ -26,6 +28,7 @@ public class EnermyCharacter : Character
     {
         base.CharacterDie();
         _animator.Play("Die");
+        _behaviour.enabled = false;
     }
     
     
